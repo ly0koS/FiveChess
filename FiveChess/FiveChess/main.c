a34aa2099d0550734e24f162f983b7295423cd1f
浴阳 沈 <ly0koS@hotmail.com>
2017-05-18 12:15:08 +0800

    已创建了棋盘，并设置了暂停

--- FiveChess/FiveChess/main.c
+++ FiveChess/FiveChess/main.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include<stdio.h>
-int Choose(int i,char map[21][21])
+int Choose(int i,int map[21][21])
 {
 	int system(const char *string);
 	while (1)
@@ -10,7 +10,7 @@ int Choose(int i,char map[21][21])
 		scanf_s("%d", &i);
 		if (i == 1)
 		{
-			DrawMap(map[21][21]);
+			DrawMap(map);
 		}
 		else if (i == 2)
 		{
@@ -23,9 +23,30 @@ int Choose(int i,char map[21][21])
 		}
 	}
 }
-int DrawMap()
+int DrawMap(int map[21][21])
 {
-
+	system("cls");
+	int i, j;
+	for (i = 0; i < 21; i++)
+	{
+		map[0][i] = i+1;
+		printf("%d    ", map[0][i]);
+	}
+	printf("\n\n\n");
+	for (i = 1; i < 21; i++)
+	{
+		map[i][0]=i+1;
+		printf("%d", map[i][0]);
+		printf("\n\n\n");
+	}
+	for (i = 1; i < 21; i++)
+	{
+		for (j = 1; j < 21; j++)
+		{
+			map[i][j] = '    ';
+		}
+	}
+	system("pause");
 }
 int PutChess()
 {
@@ -46,6 +67,6 @@ int Result()
 int x;
 int main()
 {
-	char map[21][21];
+	int map[21][21];
 	Choose(x,map);
 }
